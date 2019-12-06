aa354c452691397748ac72062e8205b9bbd5dd1e
ANDREW-PC\Andrew Richards <arich72491@gmail.com>
2019-12-06 11:19:24 -0600

    Defined Free and Used cpp files

--- Fast Memory System/Free.cpp
+++ Fast Memory System/Free.cpp
@@ -1,1 +1,9 @@
+#include "Used.h"
 #include "Free.h"
+#include "Block.h"
+
+Free::Free(uint32_t BlockSize)
+	: pFreeNext(nullptr), pFreePrev(nullptr), mBlockSize(BlockSize), mAboveBlockFree(false), pad0(0), pad1(0), mType(Block::Free)
+{
+
+}
--- Fast Memory System/Used.cpp
+++ Fast Memory System/Used.cpp
@@ -1,1 +1,8 @@
+#include "Free.h"
 #include "Used.h"
+
+Used::Used(uint32_t BlockSize)
+	: pUsedNext(nullptr), pUsedPrev(nullptr), mBlockSize(BlockSize), mAboveBlockFree(false), pad0(0), pad1(0), mType(Block::Used)
+{
+
+}
